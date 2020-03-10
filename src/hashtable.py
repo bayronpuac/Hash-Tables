da3d11b7e06a79692a102ca9dccd99f1c696a232
Bayron Puac <bayronpuac@gmail.com>
2020-03-10 15:21:15 -0500

    Updating what we did in lecture

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,12 +1,3 @@
-# '''
-# Linked List hash table key/value pair
-# '''
-class LinkedPair:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-
 class HashTable:
     '''
     A hash table that with `capacity` buckets
@@ -15,105 +6,64 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
-
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
         return hash(key)
-
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
-
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
         return self._hash(key) % self.capacity
-
-
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
-        Part 1: Hash collisions should be handled with an error warning.
-
-        Part 2: Change this so that hash collisions are handled with Linked List Chaining.
-
+        # Part 1: Hash collisions should be handled with an error warning. (Think about and
+        # investigate the impact this will have on the tests)
+        # Part 2: Change this so that hash collisions are handled with Linked List Chaining.
         Fill this in.
         '''
-        pass
-
-
-
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            print("ERROR: Key in use") 
+        else: 
+            self.storage[key] = value
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
-        pass
-
-
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            self.storage[index] = None
+        else:
+            print("WARNING: Key not found")
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
-        pass
-
-
+        index = self._hash_mod(key)
+        return self.storage[index]
     def resize(self):
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Fill this in.
         '''
-        pass
-
-
-
-if __name__ == "__main__":
-    ht = HashTable(2)
-
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
-
-    print("")
-
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
-
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
-
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
-
-    print("")
+        self.capacity = self.capacity * 2
+        prev = self.storage.copy()
+        self.storage = [None] * self.capacity
+        for item in prev:
+            self.insert(item.key, item.value)
